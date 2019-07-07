8f2ee20ee77b2d861060e40b5cb0efbffccbddb7
Akihisa Yagi <capra314cabra@gmail.com>
2019-07-07 19:17:35 +0900

    Disposed of the system of Cookie

--- src/Modules/cookie_content.ts
+++
@@ -1,9 +0,0 @@
-class SettingsContent{
-    APIKey: string;
-    ClientKey: string;
-
-    constructor(APIKey: string, clientKey: string){
-        this.APIKey = APIKey;
-        this.ClientKey = clientKey;
-    }
-}
